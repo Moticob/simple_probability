d3fe5f55d9fc7986c1a31f73f9d4ef3461dabce8
Moticob <oussama.ed-derouach@outlook.com>
2024-10-02 04:01:40 -0700

    Add probability.h: Header file for probability calculation functions.

---
+++ include/probability.h
@@ -0,0 +1,8 @@
+#ifndef PROBABILITY_H
+#define PROBABILITY_H
+
+float calculate_probability(float event_count, float total_trials);
+float calculate_joint_probability(float prob_a, float prob_b);
+
+#endif
+
